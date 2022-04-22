16e2ac27cb1425b859bfee611aef9594fc884f6c
ianashby <ianashbyphotography@gmail.com>
2022-04-22 17:30:45 -0600

    updated

--- course_portal/scripts/main.js
+++ course_portal/scripts/main.js
@@ -1,3 +1,8 @@
 // Last Modified
 let currentDate = document.lastModified;
 document.querySelector('#lastUpdated').textContent = currentDate;
+
+// Copyright Date
+let date = new Date();
+let year = date.getFullYear();
+document.querySelector('#copyrightYear').textContent = year;
