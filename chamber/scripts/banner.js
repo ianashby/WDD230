c4f1209e5567058a2eb8a62a9c3683910d334051
ianashby <ianashbyphotography@gmail.com>
2022-05-19 17:43:50 -0600

    05/19

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,11 +1,12 @@
 let day = date.getDay();
+day = 1;
 
 if (day == 1 || day == 2) {
     let banner = document.querySelector('.banner');
     banner.style.display = 'block';
 
     document.querySelector(".banner-button").addEventListener("click", function () {
-        this.closest(".banner").style.display = "none";
+        banner.style.display = 'none';
       });
 } else {
     let banner = document.querySelector('.banner');
