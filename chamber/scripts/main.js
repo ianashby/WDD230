36681a4eaa777427fc9bfc3e234d8b54170c281c
ianashby <ianashbyphotography@gmail.com>
2022-05-27 18:24:20 -0600

    05/27

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -23,7 +23,6 @@ hamburger.addEventListener('click', () => {
 });
 
 let day = date.getDay();
-day = 1;
 
 if (day == 1 || day == 2) {
     let banner = document.querySelector('.banner');
@@ -36,3 +35,18 @@ if (day == 1 || day == 2) {
     let banner = document.querySelector('.banner');
     banner.style.display = 'none';
 }
+
+// Windchill
+
+const temp = parseFloat(document.querySelector('#temp').textContent);
+const wind = parseFloat(document.querySelector('#wind').textContent);
+const windchill = document.querySelector('#windchill');
+const windDegree = document.querySelector('#windDegree');
+
+if (temp <= 50 && wind >= 3) {
+    let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(wind,0.16)) + (0.4275*temp*Math.pow(wind,0.16)));
+    windchill.textContent = chill.toFixed(0);
+    windDegree.innerHTML = '&#8457;';
+}
+
+// document.querySelector('#windchill').textContent = chill;
