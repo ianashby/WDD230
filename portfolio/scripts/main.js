320e057957639236a27f87fe945c70e9bef4d684
ianashby <ianashbyphotography@gmail.com>
2022-07-18 15:47:29 -0600

    07/18

--- portfolio/scripts/main.js
+++ portfolio/scripts/main.js
@@ -1,6 +1,3 @@
-// Prevent Right Clicking & Saving Images
-// document.addEventListener('contextmenu', event => event.preventDefault());
-
 // Hamburger Menu
 let hamburger = document.querySelector('#hamburger');
 let nav = document.querySelector('header');
@@ -8,3 +5,8 @@ let nav = document.querySelector('header');
 hamburger.addEventListener('click', () => {
     nav.classList.toggle('responsive');
 });
+
+// Copyright Date
+let date = new Date();
+let year = date.getFullYear();
+document.querySelector('#copyrightYear').innerHTML = year;
